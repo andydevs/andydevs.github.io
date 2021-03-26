7c570d019767fd12d05ec0d6d26203ec49c76461
Andydevs <akanshul97@gmail.com>
2021-03-26 17:41:51 -0400

    Used special tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ export default function Home() {
     return (
         <div>
             {/* Page header (and navigation) */}
-            <div>
+            <header>
                 {/* Navigation */}
-                <div>
+                <nav>
                     <a>Projects</a>
                     <a>Education</a>
                     <a>Skills</a>
                     <a>Work Experience</a>
                     <a>Contact</a>
-                </div>
+                </nav>
                 
                 {/* These two things are kind of together */}
                 <div>
@@ -22,91 +22,94 @@ export default function Home() {
                 
                 <p>Hobbyist developer person</p>
 
-            </div>
+            </header>
 
-            {/* Blog redirect notice */}
-            <p>
-                If you're looking for the old blog,
-                you can find it <a href="#">Here</a>
-            </p>
+            {/* Main content */}
+            <main>
+                {/* Blog redirect notice */}
+                <p>
+                    If you're looking for the old blog,
+                    you can find it <a href="#">Here</a>
+                </p>
 
-            {/* Projects */}
-            <div>
-                <h2>Projects</h2>
+                {/* Projects */}
+                <section>
+                    <h2>Projects</h2>
 
-                {/* An example project */}
-                <div>
-                    <h3>Conduit</h3>
-                    <p>
-                        (Some image or something)
-                    </p>
-                    <p>
-                        Ongoing research initiative to study
-                        temporal closed loop phenomenon known
-                        as conduits. Uses quantum computing
-                        and 4D simulation technology
-                    </p>
-                    <p>
-                        <a href="#">GitHub</a> 
-                        <a href="#">Website</a> 
-                        <a href="#">Releases</a>
-                    </p>
-                </div>
-
-            </div>
-
-            {/* Education */}
-            <div>
-                <h2>Education</h2>
-                <p>Drexel, baby</p>
-            </div>
-
-            {/* Skills */}
-            <div>
-                <h2>Skills</h2>
-                
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Programming Languages</td>
-                            <td>
-                                <ul>
-                                    <li>Python</li>
-                                    <li>C++</li>
-                                    <li>Javascript</li>
-                                </ul>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            
-            </div>
-
-            {/* Work experience */}
-            <div>
-                <h2>Work Experience</h2>
-
-                {/* Work unit */}
-                <div>
-                    {/* Title subtitle group */}
+                    {/* An example project */}
                     <div>
-                        <h3>Raytheon</h3>
-                        <h4>2020 - present</h4>
+                        <h3>Conduit</h3>
+                        <p>
+                            (Some image or something)
+                        </p>
+                        <p>
+                            Ongoing research initiative to study
+                            temporal closed loop phenomenon known
+                            as conduits. Uses quantum computing
+                            and 4D simulation technology
+                        </p>
+                        <p>
+                            <a href="#">GitHub</a> 
+                            <a href="#">Website</a> 
+                            <a href="#">Releases</a>
+                        </p>
                     </div>
-                    <ul>
-                        <li>Did some awesome stuff</li>
-                        <li>Work in a group of people</li>
-                    </ul>
-                </div>
 
-            </div>
+                </section>
 
-            {/* Contact info */}
-            <div>
-                <p>Email</p>
-                <p>Socials</p>
-                <p>Resume</p>
-            </div>
+                {/* Education */}
+                <section>
+                    <h2>Education</h2>
+                    <p>Drexel, baby</p>
+                </section>
+
+                {/* Skills */}
+                <section>
+                    <h2>Skills</h2>
+                    
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Programming Languages</td>
+                                <td>
+                                    <ul>
+                                        <li>Python</li>
+                                        <li>C++</li>
+                                        <li>Javascript</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                
+                </section>
+
+                {/* Work experience */}
+                <section>
+                    <h2>Work Experience</h2>
+
+                    {/* Work unit */}
+                    <div>
+                        {/* Title subtitle group */}
+                        <div>
+                            <h3>Raytheon</h3>
+                            <h4>2020 - present</h4>
+                        </div>
+                        <ul>
+                            <li>Did some awesome stuff</li>
+                            <li>Work in a group of people</li>
+                        </ul>
+                    </div>
+
+                </section>
+
+                {/* Contact info */}
+                <section>
+                    <p>Email</p>
+                    <p>Socials</p>
+                    <p>Resume</p>
+                </section>
+            </main>
 
             {/* Footer */}
             <footer>

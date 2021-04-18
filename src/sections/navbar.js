eecc6fac50bfb978f093c2481dfe17613f28e3bd
Andydevs <akanshul97@gmail.com>
2021-04-18 18:08:37 -0400

    Made navbar toggleable

--- src/sections/navbar.js
+++ src/sections/navbar.js
@@ -3,15 +3,25 @@ import Panel from "../components/panel";
 import "./navbar.css"
 
 export default function Navbar() {
+    // Button toggle
+    const [ toggle, setToggle ] = React.useState(false);
+    const toggleEvent = () => setToggle(!toggle);
+
     return (
         <Panel color="blue" className='navbar'>
-            <ul className='nav-list'>
-                <li className='nav-link'><a href='#projects'>Projects</a></li>
-                <li className='nav-link'><a href='#education'>Education</a></li>
-                <li className='nav-link'><a href='#skills'>Skills</a></li>
-                <li className='nav-link'><a href='#work-experience'>Work</a></li>
-                <li className='nav-link'><a href='#contact'>Contact</a></li>
-            </ul>
+            <div className='nav-header'>
+                <button onClick={toggleEvent} className='nav-toggle'>
+                    Toggle
+                </button>
+            </div>
+            {toggle &&
+                <ul className='nav-list'>
+                    <li className='nav-link'><a href='#projects'>Projects</a></li>
+                    <li className='nav-link'><a href='#education'>Education</a></li>
+                    <li className='nav-link'><a href='#skills'>Skills</a></li>
+                    <li className='nav-link'><a href='#work-experience'>Work</a></li>
+                    <li className='nav-link'><a href='#contact'>Contact</a></li>
+                </ul>}
         </Panel>
     )
 }

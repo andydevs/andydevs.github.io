8f10d6b8550002f15825abeccb7ff09dbb27f23c
andydevs <akanshul97@gmail.com>
2021-05-18 23:19:56 -0400

    Refactored hex algorithm

--- src/hooks/useHexHeroAnimation.js
+++ src/hooks/useHexHeroAnimation.js
@@ -23,40 +23,51 @@ export default function useHexHeroAnimation(radius=50, timeRate=0.01, preAlpha=0
             two.height = animationRef.current.clientHeight
         })
 
-
-        // Ease in out function
-        // 
-        // Credit to: https://easings.net/#easeInOutExpo
+        // Ease in and out functions
+        // Credit to: https://easings.net
         // Created by: Andrey Sitnik and Ivan Solovev
-        // For the function
-        const easeInOut = x => {
-            return x < 0.5 
-                ? Math.pow(2, 20 * x - 10) / 2
-                : (2 - Math.pow(2, -20 * x + 10)) / 2
-        }
+        const easeIn = (x, p=2) => Math.pow(x, p)
+        const easeOut = (x, p=2) => 1 - Math.pow(1 - x, p)
 
-        // Repeating wave pattern of signal with easing
-        const upDownSignal = (totalTime, time) => {
-            let m = 4 * (time % totalTime) / totalTime
-            return m < 1
-                ? easeInOut(m % 1)
-                : m < 2
-                ? 1
-                : m < 3
-                ? 1 - easeInOut(m % 1)
-                : 0
-        }
+        // Size signal power
+        const sizeSignalPower = 2
+        const rotationSignalPower = 3
+        const signalPeriod = 10
 
-        // Repeating stair-step pattern of signal with easing
-        const upUpSignal = (totalTime, time) => {
-            let m = 4 * (time % totalTime) / totalTime
-            return m < 1
-                ? easeInOut(m % 1)
-                : m < 2
-                ? 1
-                : m < 3
-                ? 1 + easeInOut(m % 1)
-                : 2
+        // Wave patterns for size and rotation
+        const sizeSignal = (time, period) => {
+            // Get path location in period 
+            // (broken into 4 sections)
+            let m = 4 * (time % period) / period
+            if (m < 1) { // Waiting
+                return 0
+            }
+            else if (m < 2) { // Rising
+                return easeOut(m % 1, sizeSignalPower)
+            }
+            else if (m < 3) { // Sustaining
+                return 1
+            }
+            else { // Falling
+                return 1 - easeIn(m % 1, sizeSignalPower)
+            }
+        }
+        const rotationSignal = (time, period) => {
+            // Get path location in period 
+            // (broken into 4 sections)
+            let m = 4 * (time % period) / period
+            if (m < 1) { // Waiting
+                return 0
+            }
+            else if (m < 2) { // Rising
+                return easeOut(m % 1, rotationSignalPower)
+            }
+            else if (m < 3) { // Sustaining
+                return 1
+            }
+            else { // Falling
+                return 1 + easeIn(m % 1, rotationSignalPower)
+            }
         }
 
         // Animation loop
@@ -73,16 +84,19 @@ export default function useHexHeroAnimation(radius=50, timeRate=0.01, preAlpha=0
 
                 // Go through row
                 for (let x = 0; x < two.width + radius; x += rx) {
-                    // Calculate offset parameter
-                    let a = (y/two.height + x/two.width)/2 + t
-                    let scale = preAlpha*upDownSignal(20, a)
-                    let rotation = 2*Math.PI*upUpSignal(20, a)
+                    // Calculate alpha parameter
+                    let a = (y/two.height + x/two.width)/2
+
+                    // Calculate size and rotation
+                    let scale = preAlpha*sizeSignal(a + t, signalPeriod)
+                    let rotation = 2*Math.PI*rotationSignal(a + t, signalPeriod)
 
                     // Create polygon
                     let poly = two.makePolygon(x, r, radius, 6)
                     poly.scale = scale
                     poly.rotation = rotation
                     poly.fill = '#00aaff'
+                    poly.linewidth = 0
 
                     // Update parameters
                     r += j*ry

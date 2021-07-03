91461d4de05848507a4f7a92b5719e682208a675
Andydevs <akanshul97@gmail.com>
2021-07-03 15:29:51 -0400

    Workunit table more or less...

--- src/sections/work.js
+++ src/sections/work.js
@@ -13,6 +13,35 @@ const Subtitle = styled.h4`
     font-style: italic;
 `
 
+const WorkunitTable = styled.table`
+    border-spacing: 16pt 32pt;
+    td {
+        vertical-align: top;
+        display: table-cell;
+    }
+
+    @media screen and (max-width: 700px) {
+        border-spacing: 0pt 32pt;
+        td {
+            display: block;
+        }
+    }
+`
+
+const TitleCell = styled.td`
+    width: fit-content;
+    * { margin: 4pt 0; }
+`
+
+const HighlightCell = styled.td`
+    ul {
+        margin-top: 0;
+    }
+    ul > li {
+        margin: 4pt 0;
+    }
+`
+
 export default function Work() {
     // Query
     const {
@@ -45,31 +74,34 @@ export default function Work() {
     return (
         <section id="work-experience">
             <h1>Where I've Worked</h1>
-            <Grid>
-                {workunits.map(
-                    ({ id, company, jobTitle, timeline, highlights }) => (
-                        <Panel key={id}>
-                            <Panel.Body>
-                                <Title>{company}</Title>
-                                <Subtitle>{jobTitle}</Subtitle>
-                                <Subtitle>
-                                    {timeline.start.month} {timeline.start.year}
-                                    &nbsp; &mdash; &nbsp;
-                                    {timeline.current
-                                        ? 'Present'
-                                        : `${timeline.end.month} ${timeline.end.year}`}
-                                </Subtitle>
-                                <hr />
-                                <ul>
-                                    {highlights.map((highlight, index) => (
-                                        <li key={index}>{highlight}</li>
-                                    ))}
-                                </ul>
-                            </Panel.Body>
-                        </Panel>
-                    )
-                )}
-            </Grid>
+            <WorkunitTable>
+                <tbody>
+                    {workunits.map(
+                        ({ id, company, jobTitle, timeline, highlights }) => (
+                            <tr key={id}>
+                                <TitleCell>
+                                    <Title>{company}</Title>
+                                    <Subtitle>{jobTitle}</Subtitle>
+                                    <Subtitle>
+                                        {timeline.start.month} {timeline.start.year}
+                                        &nbsp; &mdash; &nbsp;
+                                        {timeline.current
+                                            ? 'Present'
+                                            : `${timeline.end.month} ${timeline.end.year}`}
+                                    </Subtitle>
+                                </TitleCell>
+                                <HighlightCell>
+                                    <ul>
+                                        {highlights.map((highlight, index) => (
+                                            <li key={index}>{highlight}</li>
+                                        ))}
+                                    </ul>
+                                </HighlightCell>
+                            </tr>
+                        )
+                    )}
+                </tbody>
+            </WorkunitTable>
         </section>
     )
 }

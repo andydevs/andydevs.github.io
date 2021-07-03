ee4f85068fd5f31f14a1283b7efbd0089adc207f
Andydevs <akanshul97@gmail.com>
2021-07-03 16:31:30 -0400

    Add itch.io profile

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import IconLink from '../components/iconlink'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faItchIo } from '@fortawesome/free-brands-svg-icons'
 import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
@@ -34,6 +34,10 @@ export default function Footer() {
                     href={`https://www.github.com/${contact.github}`}
                 />
                 <IconLink large icon={faLinkedin} href={contact.linkedin} />
+                <IconLink
+                    large
+                    icon={faItchIo}
+                    href={`${contact.itch}.itch.io`}/>
                 <IconLink
                     large
                     icon={faEnvelope}

e982142e9f6d1547fe43c189b28ebf8f1efde301
Andydevs <akanshul97@gmail.com>
2021-11-14 22:51:08 -0500

    Update title

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,42 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
-import Banner from '../assets/graphics/banner.svg'
 
 const HeroDiv = styled.div`
     position: relative;
     height: 100vh;
+    width: 100vw;
     overflow: hidden;
 `
 
-const HeroBannerContainer = styled.div`
+const HeroContainer = styled.div`
     position: absolute;
     top: 50%;
     right: 10%;
-    @media screen and (max-width: 1400px) {
+    @media screen and (max-width: 700px) {
         right: 0%;
     }
     transform: translateY(-50%);
-    text-align: right;
-    height: 100%;
-    width: 100%;
+    text-align: left;
+    padding: 0pt 16pt;
 `
 
-const HeroBanner = styled(Banner)`
-    width: 100%;
-    text-align: right;
-    max-height: 100%;
-    max-width: 1280px;
+const HeroTitle = styled.h1`
+    margin: 0;
+    padding: 0;
+
+    font-size: 80pt;
+    @media screen and (max-width: 1400px) {
+        font-size: 40pt;
+    }
+`
+
+const HeroSubtitle = styled.h2`
+    margin: 0;
+
+    font-size: 40pt;
+    @media screen and (max-width: 1400px) {
+        font-size: 20pt;
+    }
+
+    &:before {
+        content: '';
+    }
 `
 
 export default function Hero() {
     return (
         <HeroDiv>
-            <HeroBannerContainer>
-                <HeroBanner
-                    data-sal="slide-up"
-                    data-sal-duration="500"
-                    data-sal-easing="ease"/>
-            </HeroBannerContainer>
+            <HeroContainer>
+                <HeroTitle>Anshul Kharbanda</HeroTitle>
+                <HeroSubtitle>Andydevs</HeroSubtitle>
+            </HeroContainer>
         </HeroDiv>
     )
 }

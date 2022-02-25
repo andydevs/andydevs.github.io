b27b34de8beaa242806ca57a05c3fb810fa5f187
Andydevs <akanshul97@gmail.com>
2022-02-24 23:45:31 -0500

    Add logo to hero title

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Logo from '../assets/graphics/logo.svg';
 
 const HeroDiv = styled.div`
     position: relative;
@@ -18,29 +19,60 @@ const HeroContainer = styled.div`
     transform: translateY(-50%);
     text-align: left;
     padding: 0pt 16pt;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    @media screen and (max-width: 700px) {
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+const HeroLogoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+    width: 200px;
+    @media screen and (max-width: 1400px) {
+        height: 100px;
+        width: 100px;
+    }
+    @media screen and (max-width: 700px) {
+        height: 150px;
+        width: 150px;
+    }
+`
+
+const HeroText = styled.div`
+    @media screen and (max-width: 700px) {
+        text-align: center;
+    }
 `
 
 const HeroTitle = styled.h1`
     margin: 0;
     padding: 0;
-
     font-size: 80pt;
     @media screen and (max-width: 1400px) {
         font-size: 46pt;
     }
     @media screen and (max-width: 700px) {
         font-size: 40pt;
+        margin-bottom: 10pt;
+    }
+    &:before {
+        content: '';
     }
 `
 
 const HeroSubtitle = styled.h2`
     margin: 0;
-
     font-size: 40pt;
     @media screen and (max-width: 1400px) {
         font-size: 20pt;
     }
-
     &:before {
         content: '';
     }
@@ -50,19 +82,29 @@ export default function Hero() {
     return (
         <HeroDiv>
             <HeroContainer>
-                <HeroTitle
-                    data-sal="slide-up"
-                    data-sal-duration="500"
-                    data-sal-easing="ease">
-                        Anshul Kharbanda
-                </HeroTitle>
-                <HeroSubtitle
-                    data-sal="slide-up"
-                    data-sal-duration="500"
-                    data-sal-easing="ease"
-                    data-sal-delay="100">
-                        Andydevs
-                </HeroSubtitle>
+                <HeroLogoContainer>
+                    <Logo
+                        data-sal="slide-up"
+                        data-sal-duration="500"
+                        data-sal-easing="ease"
+                        data-sal-delay="100"/>
+                </HeroLogoContainer>
+                <HeroText>
+                    <HeroTitle
+                        data-sal="slide-up"
+                        data-sal-duration="500"
+                        data-sal-easing="ease"
+                        data-sal-delay="200">
+                            Anshul Kharbanda
+                    </HeroTitle>
+                    <HeroSubtitle
+                        data-sal="slide-up"
+                        data-sal-duration="500"
+                        data-sal-easing="ease"
+                        data-sal-delay="300">
+                            Andydevs
+                    </HeroSubtitle>
+                </HeroText>
             </HeroContainer>
         </HeroDiv>
     )

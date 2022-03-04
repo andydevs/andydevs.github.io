ca60877ae811d43c1319108c94abf09145aaa245
Andydevs <akanshul97@gmail.com>
2022-03-04 14:26:53 -0500

    Made two copies of logo so one can be for favicon

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Logo from '../assets/graphics/logo.svg'
+import Logo from '../assets/graphics/logo-banner.svg'
 
 const HeroBackground = styled.div`
     position: relative;

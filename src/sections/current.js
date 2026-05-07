ec4830df4cb77727df7415212c926ff3bfd6d450
Anshul Kharbanda <akanshul97@gmail.com>
2026-05-07 00:37:06 -0400

    Included the ominous ring ooOOooOOOooo

--- src/sections/current.js
+++ src/sections/current.js
@@ -5,7 +5,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Section from '../components/section'
-import { PieStencil } from 'reactive-charts/src/PieChart'
+import { PieStencil } from 'reactive-charts'
+
+const degrees = Math.PI / 180
+
+const pieStyle = {
+    svg: { width: 900, height: 600 },
+    maxCategories: 10,
+    categoryCollapse: { label: { text: 'others...', color: '#666', fontStyle: 'italic' }, minAngle: 10 * degrees },
+    sliceAngleGap: 1 * degrees,
+    sliceMaxRadius: 210,
+    sliceThickness: 50,
+    sliceCornerRadius: 3,
+    labelOffset: 35,
+    labelPadding: 4,
+    labelHeight: 2
+}
 
 export default function Current() {
     return (
@@ -18,7 +33,7 @@ export default function Current() {
             >
                 What I've Been Doing
             </h1>
-            <PieStencil />
+            <PieStencil style={pieStyle} />
         </Section>
     )
 }

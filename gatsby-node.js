dc652eacc27f3f613e40a83a4366f665b390712a
Anshul Kharbanda <akanshul97@gmail.com>
2026-06-05 18:39:25 -0400

    Hook github data to pie chart

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,74 @@
 const axios = require('axios')
 
+async function sourceGithub(createNode, createNodeId, createContentDigest) {
+    let query = /* GraphQL */ `
+        {
+            viewer {
+                repositories(
+                    first: 10
+                    orderBy: { field: PUSHED_AT, direction: DESC }
+                    ownerAffiliations: [OWNER]
+                    isFork: false
+                ) {
+                    nodes {
+                        name
+                        pushedAt
+                        languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
+                            edges {
+                                size
+                                node {
+                                    name
+                                    color
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `
+
+    const response = await axios.post(
+        'https://api.github.com/graphql',
+        { query },
+        { headers: { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` } }
+    )
+
+    const repos = response.data.data.viewer.repositories.nodes.map(repo => {
+        const languages = repo.languages.edges.map(edge => ({
+            name: edge.node.name,
+            color: edge.node.color,
+            size: edge.size
+        }))
+        return { name: repo.name, pushedAt: repo.pushedAt, languages }
+    })
+
+    for (const repo of repos) {
+        let contentDigest = createContentDigest(repo)
+        createNode({
+            ...repo,
+            parent: null,
+            children: [],
+            id: createNodeId(`GitHubRepo-${repo.name}`),
+            internal: { type: 'GithubRepo', contentDigest }
+        })
+    }
+}
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+    const { createNode } = actions
+    sourceGithub(createNode, createNodeId, createContentDigest)
+}
+
 exports.createSchemaCustomization = ({ actions }) => {
     const { createTypes } = actions
-    const typeDefs = `
+    const typeDefs = /* GraphQL */ `
         type ContentYamlProjects {
-            id: Int,
-            title: String,
+            id: Int
+            title: String
             image: File @fileByRelativePath
-            description: String,
-            github: String,
+            description: String
+            github: String
             website: String
         }
     `
--- src/sections/current.js
+++ src/sections/current.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Section from '../components/section'
-import { PieStencil } from 'reactive-charts'
+import { PieChart } from 'reactive-charts'
 
 const degrees = Math.PI / 180
 
 const pieStyle = {
     svg: { width: 900, height: 600 },
     maxCategories: 10,
-    categoryCollapse: { label: { text: 'others...', color: '#666', fontStyle: 'italic' }, minAngle: 10 * degrees },
+    categoryCollapse: { label: { text: 'others...', color: '#666', fontStyle: 'italic' }, minAngle: 3 * degrees },
     sliceAngleGap: 1 * degrees,
     sliceMaxRadius: 210,
     sliceThickness: 50,
@@ -23,17 +19,56 @@ const pieStyle = {
 }
 
 export default function Current() {
+    let {
+        allGithubRepo: { nodes: repos }
+    } = useStaticQuery(graphql`
+        query GithubQuery {
+            allGithubRepo {
+                nodes {
+                    name
+                    languages {
+                        name
+                        color
+                        size
+                    }
+                }
+            }
+        }
+    `)
+
+    let languages = repos
+        .flatMap(repo => repo.languages)
+        .reduce((obj, language) => {
+            if (!obj[language.name]) {
+                obj[language.name] = { color: language.color, size: 0 }
+            }
+            obj[language.name].size += language.size
+            return obj
+        }, {})
+
+    let labels = Object.entries(languages).map(([name, { color, size }]) => ({
+        label: {
+            text: name,
+            color: color,
+            fontStyle: 'normal'
+        },
+        value: size
+    }))
+    console.log(labels)
+
     return (
         <Section id="current">
-            <h1
+            <h1 data-sal="slide-up" data-sal-delay="300" data-sal-duration="500" data-sal-easing="ease">
+                What I've Been Working With
+            </h1>
+            <PieChart
                 data-sal="slide-up"
                 data-sal-delay="300"
                 data-sal-duration="500"
                 data-sal-easing="ease"
-            >
-                What I've Been Doing
-            </h1>
-            <PieStencil style={pieStyle} />
+                style={pieStyle}
+                categories={labels}
+            />
         </Section>
     )
 }
